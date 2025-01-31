a1b5282c45295bb9b65cc7e476819a50d066c8a1
dongseok-kim <kdsehdtjr12@gmail.com>
2025-01-31 14:43:58 +0900

    Make Item
    Item스크립트생성

---
+++ TextRPG/Gamemanager.cs
@@ -0,0 +1,15 @@
+﻿namespace TextRPG
+{
+    internal class Gamemanager
+    {
+        static void Main(string[] args)
+        {
+            Item i = new Item();
+            
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
